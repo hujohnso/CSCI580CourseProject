88dc28b0f200498cd2f0603c25165bebdc7396eb
hujohnso <hujohnso@mymail.mines.edu>
2018-11-04 16:35:54 -0700

    Finished a rough version multi constant version

--- solvers/FirstOrderODE_Analytical.py
+++ solvers/FirstOrderODE_Analytical.py
@@ -15,20 +15,31 @@ trainUpperBound = 5
 trainLowerBound = 1
 testUpperBound = 6
 testLowerBound = 4
-trainDataSize = 300
+trainDataSize = 5
 testDataSize = 100
-testConstants = None
-trainConstants = None
+#rows will be the particular constants and columns will be each iteration
+testConstants = np.array([[3],[4]])
+trainConstants = np.array([[1],[2],[3]])
 constant = None
-odeClassToUse = ode.ex
+odeClassToUse = ode.x2
 
-# Neural Network Generator
+#This looks a little confusing but all it is doing is combining the x_Train_Matrix with the proper constants
+#that correspond to the evaluation
+def pairConstantsMatrixAndXMatrixForDNNInput(x_Matrix, constantsMatrix):
+    constantsLength = getConstantsLength(constantsMatrix)
+    input_Matrix = y = np.zeros(shape=(constantsMatrix.shape[1] + 1, len(x_Matrix) * constantsLength))
+    input_Matrix[0,:] = x_Matrix.flatten('F')
+    constantsExpandedMatrix = np.zeros(shape = (constantsMatrix.shape[1],len(x_Matrix) * constantsLength))
+    for i in range(constantsMatrix.shape[1]):
+        for j in range(constantsLength):
+            input_Matrix[i + 1,len(x_Matrix) * j:len(x_Matrix) * (j + 1)] = np.repeat(constantsMatrix[j,:],len(x_Matrix))
+    return input_Matrix
 def generatePrediction(myODE):
 
     get_custom_objects().update({'custom_activation': Activation(myODE.custom_activation)})
 
     model = Sequential()
-    model.add(Dense(1, activation='relu', input_dim = 1))
+    model.add(Dense(1, activation='relu', input_dim = trainConstants.shape[1] + 1))
     model.add(Dense(10, activation='relu'))
     model.add(Dense(10, activation='relu'))
     model.add(Dense(1, activation='custom_activation'))
@@ -38,49 +49,64 @@ def generatePrediction(myODE):
     model.compile(loss='mean_squared_error',
               optimizer='adam',
               metrics=['accuracy'])
+    
+    x_input_for_model = pairConstantsMatrixAndXMatrixForDNNInput(myODE.x_train,trainConstants).transpose()
+    model.fit(x_input_for_model, myODE.y_train.flatten('C').reshape(-1,1), epochs=20, batch_size=2, verbose=0)
+    x_input_for_model = pairConstantsMatrixAndXMatrixForDNNInput(myODE.x_test, testConstants).transpose()
+    y_pred = model.predict(x_input_for_model)
+    return y_pred.reshape(x_test.shape[0],x_test.shape[1],order='F')
 
-    model.fit(myODE.x_train, myODE.y_train, epochs=20, batch_size=2, verbose=0)
-
-    #score = model.evaluate(x_test, y_test, verbose=1)  #print(score)
-
-    # y_pred contains the prediction with the x_test input
-    y_pred = model.predict(myODE.x_test)
-    return y_pred
-
-# Plotting function
+def plotMatrixData(x_test, y_test, line, labelValue):
+    for i in range(x_test.shape[1]):
+        plt.plot(x_test[:,i].transpose(),y_test[:,i].transpose(), line , label = labelValue)
+ #y_pred.reshape(x_test.shape[0],x_test.shape[1],order='F')
 def plot(myODE, y_pred):
     tfit = np.linspace(testLowerBound, testUpperBound, num=testDataSize).reshape(-1, 1)
-    plt.plot(tfit, y_pred, label='soln')
-    plt.plot(tfit, myODE.y_test, 'r--', label='analytical soln')
+    plotMatrixData(myODE.x_test,myODE.y_test, 'r--', 'analytical soln')
+    plotMatrixData(myODE.x_test,y_pred,'b--','DNN soln')
     plt.legend()
     plt.xlabel('x')
     plt.ylabel('y')
     plt.xlim([testLowerBound, testUpperBound])
     plt.savefig('image.png')
 
+def calculateError(y_pred, myODE):
     localError = 0
-    for i in range(0, len(y_pred)):
-        localError += abs((y_pred[i] - myODE.y_test[i]) /myODE. y_test[i]) * 100
-    avgError = localError / len(y_pred)
+    for i in range(myODE.y_test.shape[0]):
+        for j in range(myODE.y_test.shape[1]):
+            localError += abs((y_pred[i,j] - myODE.y_test[i,j]) /myODE. y_test[i,j]) * 100
+    avgError = localError / (myODE.y_test.size)
     print("On average, the error is: ", avgError, " %")
+    
+def initialize_X_Array(lowerBound, upperBound, dataSize , constants):
+    constantsLength = getConstantsLength(constants)
+    x = np.linspace(lowerBound, upperBound, num= dataSize)
+    x.shape = len(x),1
+    x_array = np.zeros(shape=(len(x),constantsLength))
+    for j in range(0, constantsLength):
+        x_array[:,j] = list(x)
+    return x_array
 
-# Main
+#This is necessary to incorporate the case in which constants is set to none
+def getConstantsLength(constants):
+    if constants is None:
+        constantsLength = 1
+    else:
+        constantsLength = constants.shape[0]
+    return constantsLength
+
 if __name__ == "__main__":
     numpy.random.seed(7)
-    x_train = np.linspace(trainLowerBound, testUpperBound, num=trainDataSize)
-    x_train.shape = trainDataSize, 1
-    x_test = np.linspace(testLowerBound, testUpperBound, num=testDataSize)
-    x_test.shape = testDataSize, 1
-    
-    if constant is None:
-        myODE = odeClassToUse(x_test, x_train)
-    else:
-        myODE = odeClassToUse(x_test, x_train,constant) 
-    myODE.y_test = odeClassToUse.func(myODE,x_test)
-    myODE.y_train = odeClassToUse.func(myODE, x_train)
+    x_train = initialize_X_Array(trainLowerBound,trainUpperBound,trainDataSize,trainConstants)
+    x_test = initialize_X_Array(testLowerBound,testUpperBound,testDataSize,testConstants)
+    myODE = odeClassToUse(x_test, x_train)
+    myODE.y_train = odeClassToUse.func(myODE, x_train,trainConstants)
+    myODE.y_test = odeClassToUse.func(myODE,x_test,testConstants)
 
-    # Generate the prediction provided the training information
     y_pred = generatePrediction(myODE)
 
-    # Plot the results
     plot(myODE, y_pred)
+    
+    calculateError(y_pred,myODE)
+    
+    
--- solvers/differential_equations/AnalyticalODE1.py
+++ solvers/differential_equations/AnalyticalODE1.py
@@ -2,38 +2,40 @@ import autograd.numpy as np
 import tensorflow as tf
 
 class analyticalODE1:
-    constant = None
-    def __init__ (self, x_test, x_train, constant = 1):
+    def __init__ (self, x_test, x_train):
         self.x_test = x_test
         self.x_train = x_train
-        self.constant = constant
         
     #I didn't feel like repeating the logic for all of the seperate functions
     #so here is a generic evaluator for all of the sub classes to use :)
-    def performAnalyticalFunction(self, functionToPerform ,x , constants):
-        y = np.linspace(0,0,num = len(x))
-        y.shape = len(x), 1
-        for i in range(0, len(x)):
-            y[i] = functionToPerform(x[i],constants)
+    def performAnalyticalFunction(self ,functionToPerform, x , constants):
+        if constants is None:
+            constantsLength = 1
+        else:
+            constantsLength = constants.shape[0]
+        y = np.zeros(shape=(len(x),constantsLength))
+        for j in range(0, constantsLength):
+            for i in range(0, len(x)):
+                y[i,j] = functionToPerform(x[i,j],constants[j,:])
         return y
-
+    
 class x2(analyticalODE1):
-    def __init__(self, x_test, x_train,constant = 1):
-        super().__init__(x_test, x_train,constant)
+    def __init__(self, x_test, x_train):
+        super().__init__(x_test, x_train)
                          
-    def func(self,x):
-        return super(x2, self).performAnalyticalFunction(lambda value, constant: constant * (value ** 2 / 2), x, self.constant)
+    def func(self,x,constants=np.array(1)):
+        return super(x2, self).performAnalyticalFunction(lambda value, constant: constant * (value ** 2 / 2), x, constants)
               
     @staticmethod
     def custom_activation(x):
         return x ** 2
 
 class x4(analyticalODE1):
-    def __init__(self, x_test, x_train, constant = np.array([1,1,1])):
-        super().__init__(x_test, x_train, constant)
+    def __init__(self, x_test, x_train):
+        super().__init__(x_test, x_train)
     
-    def func(self,x):
-        return super(x4, self).performAnalyticalFunction(lambda value ,constant:(constant[0] * (value ** 4 / 4)) + constant[1] * (value ** 3 / 3) + constant[2] * (value ** 2 / 2), x, self.constant)
+    def func(self,x,constants=np.array([1,1,1])):
+        return super(x4, self).performAnalyticalFunction(lambda value ,constant:(constant[0] * (value ** 4 / 4)) + constant[1] * (value ** 3 / 3) + constant[2] * (value ** 2 / 2), x, constants)
     
     @staticmethod
     def custom_activation(x):
@@ -41,11 +43,11 @@ class x4(analyticalODE1):
 
 
 class ex(analyticalODE1):
-    def __init__(self, x_test, x_train, constant = 1):
-        super().__init__(x_test, x_train, constant)
+    def __init__(self, x_test, x_train):
+        super().__init__(x_test, x_train)
 
-    def func(self, x):
-        return super(ex, self).performAnalyticalFunction(lambda value, constant: constant * np.exp(value), x, self.constant)
+    def func(self, x,constants=np.array([1])):
+        return super(ex, self).performAnalyticalFunction(lambda value, constant: constant * np.exp(value), x, constants)
 
     @staticmethod
     def custom_activation(x):
@@ -53,11 +55,11 @@ class ex(analyticalODE1):
 
 
 class log(analyticalODE1):
-    def __init__(self, x_test, x_train,constant = 1):
-        super().__init__(x_test, x_train,constant)
+    def __init__(self, x_test, x_train):
+        super().__init__(x_test, x_train)
 
-    def func(self, x):
-        return super(log, self).performAnalyticalFunction(lambda value,constant: constant * log(value), x, self.constant)
+    def func(self, x,constants=np.array([1])):
+        return super(log, self).performAnalyticalFunction(lambda value,constant: constant * log(value), x, constants)
     
     @staticmethod
     def custom_activation(x):
