85a8c416b0b319dd20f2a1870f565ffdf9061687
krampcg23 <krampcg23@gmail.com>
2018-11-08 16:31:10 -0700

    added two new functions

--- solvers/differential_equations/FinDiffODE1.py
+++ solvers/differential_equations/FinDiffODE1.py
@@ -9,6 +9,15 @@ class finDiffODE1:
         self.x_test = x_test
         self.x_train = x_train
 
+    def func(self, x):
+        t = copy.deepcopy(x)
+        t = t.reshape(1, len(x))
+        t = t[0]
+        y0 = 1 # initial condition
+        y = odeint(self.model, y0, t)
+        y.shape = len(x), 1
+        return y
+
 # Test with analytical DE, dy/dt = -k*y
 # Analytical solution is y = ce^{-kt}
 class neg_ex(finDiffODE1):
@@ -21,13 +30,7 @@ class neg_ex(finDiffODE1):
         return dydt
 
     def func(self, x):
-        t = copy.deepcopy(x)
-        t = t.reshape(1, len(x))
-        t = t[0]
-        y0 = 1 # initial condition
-        y = odeint(self.model, y0, t)
-        y.shape = len(x), 1
-        return y
+        return super(neg_ex, self).func(x)
 
     @staticmethod
     def custom_activation(x):
@@ -43,15 +46,40 @@ class neg_ex_2(finDiffODE1):
         return dydt
 
     def func(self, x):
-        t = copy.deepcopy(x)
-        t = t.reshape(1, len(x))
-        t = t[0]
-        y0 = 0  # initial condition
-        y = odeint(self.model, y0, t)
-        y.shape = len(x), 1
-        return y
+        return super(neg_ex_2, self).func(x)
 
     @staticmethod
     def custom_activation(x):
         return 3 - tf.nn.relu(x)
         return 3 - tf.math.exp(-x)
+
+class x2(finDiffODE1):
+    def __init__(self, x_test, x_train):
+        super().__init__(x_test, x_train)
+
+    def model(self, y, t):
+        dydt = t*(1+(np.abs(t)))
+        return dydt
+
+    def func(self, x):
+        return super(x2, self).func(x)
+
+    @staticmethod
+    def custom_activation(x):
+        return x ** 2
+
+
+class abssin(finDiffODE1):
+    def __init__(self, x_test, x_train):
+        super().__init__(x_test, x_train)
+
+    def model(self, y, t):
+        dydt = t*np.abs(np.sin(t))
+        return dydt
+
+    def func(self, x):
+        return super(abssin, self).func(x)
+
+    @staticmethod
+    def custom_activation(x):
+        return x**2 + tf.cos(x) * x
