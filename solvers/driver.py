603b47924c49f747aee393131846803028f23b80
krampcg23 <krampcg23@gmail.com>
2018-11-08 17:34:53 -0700

    adjusted so that findiff takes in params

--- solvers/driver.py
+++ solvers/driver.py
@@ -8,7 +8,7 @@ import time
 
 trainUpperBound = 10
 trainLowerBound = 0
-testUpperBound = 15
+testUpperBound = 10
 testLowerBound = 0
 trainDataSize = 500
 testDataSize = 200
@@ -20,10 +20,10 @@ testDataSize = 200
 #trainConstants = np.array([[1,1,1], [1,1,5],[1,5,1],[5,1,1], [10,5,5], [5,10,5],[5,5,10], [5,5,5]])
 # ClassToUse = ode.x4
 
-testConstants = np.array([[1]])
-trainConstants = np.array([[1]])
+testConstants = np.array([[4.9]])
+trainConstants = np.array([[4.8], [4.9], [5]])
 #ClassToUse = ode.x2
-ClassToUse = findiffODE.neg_ex
+ClassToUse = findiffODE.x2
 
 # testConstants = np.array([[3],[4],[5]])
 # trainConstants = np.array([[3],[4],[5]])
@@ -40,9 +40,9 @@ if __name__ == "__main__":
     x_test = initialize_X_Array(testLowerBound,testUpperBound,testDataSize,testConstants)
     myODE = ClassToUse(x_test, x_train)
 
-    myODE.y_train = ClassToUse.func(myODE, x_train) #, x_train)#,trainConstants)
+    myODE.y_train = ClassToUse.func(myODE, x_train, trainConstants)
     startFinDiff = time.clock()
-    myODE.y_test = ClassToUse.func(myODE, x_test) #,x_test)#,testConstants)
+    myODE.y_test = ClassToUse.func(myODE, x_test ,testConstants)
     print("Total time for finite difference approximation is: ", time.clock() - startFinDiff)
 
     #x_train.shape = trainDataSize, 1
